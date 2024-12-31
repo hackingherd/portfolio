1ba463eb12eeb07aebcb9e4052e5af9e2ecad1d6
hackingherd <121489703+hackingherd@users.noreply.github.com>
2024-12-31 15:03:54 +0530

    AboutCard.js

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -8,8 +8,8 @@ function AboutCard() {
       <Card.Body>
         <blockquote className="blockquote mb-0">
           <p style={{ textAlign: "justify" }}>
-            Hi Everyone, I am <span className="purple">Soumyajit Behera </span>
-            from <span className="purple"> Bhubaneswar, India.</span>
+            Hi Everyone, I am <span className="purple">Kondasani Malathi </span>
+            from <span className="purple"> Sri Sathya Sai, AP, India.</span>
             <br />
             I am currently employed as a software developer at Juspay.
             <br />
@@ -19,10 +19,7 @@ function AboutCard() {
             <br />
             Apart from coding, some other activities that I love to do!
           </p>
-          <ul>
-            <li className="about-activity">
-              <ImPointRight /> Playing Games
-            </li>
+          <ul?
             <li className="about-activity">
               <ImPointRight /> Writing Tech Blogs
             </li>
@@ -34,7 +31,7 @@ function AboutCard() {
           <p style={{ color: "rgb(155 126 172)" }}>
             "Strive to build things that make a difference!"{" "}
           </p>
-          <footer className="blockquote-footer">Soumyajit</footer>
+          <footer className="blockquote-footer">Kondasani Malathi</footer>
         </blockquote>
       </Card.Body>
     </Card>
